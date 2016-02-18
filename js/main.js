15c13b5e58cb912f6240d7750e6e8439843d0364
魏展廷 <tim@4point-inc.com>
2016-02-18 14:54:25 +0800

    add port

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('.main_container').ready(function{
   function get_data(e){
     $.ajax({
       type: "GET",
-      url: "http://192.168.1.103/api/get_main",
+      url: "http://192.168.1.103:3000/api/get_main",
       timeout: 5000,
       success: function(json) {
         e = json;
