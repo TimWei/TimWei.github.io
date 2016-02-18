ab1351b412b5d2a8884475de5ddda3f50273d64d
魏展廷 <tim@4point-inc.com>
2016-02-18 15:35:51 +0800

    testing append

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 $('.main_container').ready(function(){
-  var data;
-  get_data(data);
-  console.log(data);
+  get_data();
 
-
-  function get_data(e){
+  function get_data(){
     $.ajax({
       type: "GET",
       url: "http://192.168.1.103:3000/api/get_main",
       timeout: 5000,
       success: function(json) {
-        e = json;
+        set_list(json);
       },
       error: function(){
-        e = 'timeout';
+        return 'timeout';
       }
     });
+  }
+
+  function set_list(data){
+    var list = $('.list');
+    for(var key in data){
+      if(data[key].length > 0){
+        append_list_item(list,key);
+      }else{
+        continue;
+      }
+    }
+  } 
+
+  function append_list_item(parent,key){
+    var list_item = '<div class="list_item">'+
+                      '<div style="padding-top:50px;">'+
+                        '<img style="width:100px;" src="img/" '+ key + '.png">'+
+                        '<span style="font-size: 25px;">' + key + '</span>'
+                      '</div>'+
+                    '</div>';
+    parent.append(list_item);
   }
 
 });
