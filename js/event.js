74ee16482003be72c0eeff7f5447528aad910ca0
ZhantMajaja <aaasdslee@gmail.com>
2016-06-23 23:13:37 +0800

    add ex

--- js/event.js
+++ js/event.js
@@ -17,21 +17,32 @@ var BOSS = [
   'ティターン',
   'アグニス',
   'ゼピュロス',
-  'ネプチューン'
+  'ネプチューン',
+  '玄武',
+  '朱雀',
+  '白虎',
+  '青竜'
   ]
 
 var BOSS_IMG_HASH = {
   'ティターン': 'img/event/four_earth.png',
   'アグニス': 'img/event/four_fire.png',
   'ゼピュロス': 'img/event/four_wind.png',
-  'ネプチューン': 'img/event/four_water.png'
-
+  'ネプチューン': 'img/event/four_water.png',
+  '玄武': 'img/event/four_water_ex.png',
+  '朱雀': 'img/event/four_fire_ex.png',
+  '白虎': 'img/event/four_earth_ex.png',
+  '青竜': 'img/event/four_wind_ex.png'
 }
 var BOSS_NAME_HASH = {
   'ティターン': 'four_earth',
   'アグニス': 'four_fire',
   'ゼピュロス': 'four_wind',
-  'ネプチューン': 'four_water'
+  'ネプチューン': 'four_water',
+  '玄武':'four_water_ex',
+  '朱雀':'four_fire_ex',
+  '白虎':'four_earth_ex',
+  '青竜':'four_wind_ex'
 }
 
 
