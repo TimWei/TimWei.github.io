0bf5a6d23b94a8c096271a1f2ff1a651a275466c
tim <tim.w@axsoftware.net>
2016-07-01 15:26:49 +0800

    改一下JS結構 + Ajax tick tock

---
+++ js/setting/set_event.js
@@ -0,0 +1,38 @@
+
+var HOST_IP = 'http://gacha-blue-fantasy.herokuapp.com';
+var SERVICE_URL =  HOST_IP + "/api/get_event";
+    
+//constants
+var BOSS = [
+  'ティターン',
+  'アグニス',
+  'ゼピュロス',
+  'ネプチューン',
+  '玄武',
+  '朱雀',
+  '白虎',
+  '青竜'
+  ]
+
+var BOSS_IMG_HASH = {
+  'ティターン': 'img/event/four_earth.png',
+  'アグニス': 'img/event/four_fire.png',
+  'ゼピュロス': 'img/event/four_wind.png',
+  'ネプチューン': 'img/event/four_water.png',
+  '玄武': 'img/event/four_water_ex.png',
+  '朱雀': 'img/event/four_fire_ex.png',
+  '白虎': 'img/event/four_earth_ex.png',
+  '青竜': 'img/event/four_wind_ex.png'
+}
+var BOSS_NAME_HASH = {
+  'ティターン': 'four_earth',
+  'アグニス': 'four_fire',
+  'ゼピュロス': 'four_wind',
+  'ネプチューン': 'four_water',
+  '玄武':'four_water_ex',
+  '朱雀':'four_fire_ex',
+  '白虎':'four_earth_ex',
+  '青竜':'four_wind_ex'
+}
+
+var EMPTY_STRING = '目前沒活動唷!';
---
+++ js/setting/set_hl.js
@@ -0,0 +1,54 @@
+
+var HOST_IP = 'http://gacha-blue-fantasy.herokuapp.com';
+var SERVICE_URL =  HOST_IP + "/api/get_hl";
+
+//constants
+var BOSS = [
+  'ローズクイーン',
+  'ナタク',
+  'フラム＝グラス',
+  'マキュラ・マリウス',
+  'メドゥーサ',
+  'アポロン',
+  'Dエンジェル・オリヴィエ',
+  'ティアマト・マグナ＝エア',
+  'コロッサス・マグナ',
+  'リヴァイアサン・マグナ',
+  'ユグドラシル・マグナ',
+  'シュヴァリエ・マグナ',
+  'セレスト・マグナ'
+  ]
+
+var BOSS_IMG_HASH = {
+  'ローズクイーン': 'img/hl_main/rosen.png',
+  'ナタク': 'img/hl_main/wind.png',
+  'フラム＝グラス': 'img/hl_main/fire.png',
+  'マキュラ・マリウス': 'img/hl_main/water.png',
+  'メドゥーサ': 'img/hl_main/earth.png',
+  'アポロン': 'img/hl_main/light.png',
+  'Dエンジェル・オリヴィエ': 'img/hl_main/dark.png',
+  'ティアマト・マグナ＝エア': 'img/main/wind.png',
+  'コロッサス・マグナ': 'img/main/fire.png',
+  'リヴァイアサン・マグナ': 'img/main/water.png',
+  'ユグドラシル・マグナ': 'img/main/earth.png',
+  'シュヴァリエ・マグナ': 'img/main/light.png',
+  'セレスト・マグナ': 'img/main/dark.png',
+}
+
+var BOSS_NAME_HASH = {
+  'ローズクイーン': 'rosen',
+  'ナタク': 'wind_hl',
+  'フラム＝グラス': 'fire_hl',
+  'マキュラ・マリウス': 'water_hl',
+  'メドゥーサ': 'earth_hl',
+  'アポロン': 'light_hl',
+  'Dエンジェル・オリヴィエ': 'dark_hl',
+  'ティアマト・マグナ＝エア': 'wind',
+  'コロッサス・マグナ': 'fire',
+  'リヴァイアサン・マグナ': 'water',
+  'ユグドラシル・マグナ': 'earth',
+  'シュヴァリエ・マグナ': 'light',
+  'セレスト・マグナ': 'dark'
+}
+
+var EMPTY_STRING = '目前沒HL唷!';
